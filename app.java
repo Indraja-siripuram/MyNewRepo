6d0afd3bf38c124ce3b4af3337784ba91743a9d7
Indraja <105694504+Indraja-siripuram@users.noreply.github.com>
2026-02-13 13:31:28 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,18 @@
 								 //     }
 								 //     }
 								 //
+
+			
+//dev-2 push from local
+
+	public class AddNumbers {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        int sum;
+
+        sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
