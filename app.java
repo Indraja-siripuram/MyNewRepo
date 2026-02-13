34772c428796d3dfc2290f350daec367d2dbf1dc
root <root@ip-172-31-21-185.ec2.internal>
2026-02-13 07:52:44 +0000

    java code added

---
+++ app.java
@@ -0,0 +1,6 @@
+[200~public class HelloWorld {
+	    public static void main(String[] args) {
+		            System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+								 //     }
+								 //     }
+								 //
